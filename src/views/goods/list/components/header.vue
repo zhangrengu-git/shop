eeba0c34268eff4b2a928994a56ba6937df62ddf
zhangrengu <1005006793@qq.com>
2022-03-07 23:06:31 +0800

    修改头部选项、完善静态商品列表

--- src/views/goods/list/components/header.vue
+++ src/views/goods/list/components/header.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="container">
-    <el-form :inline="true" :model="form" ref="form" style="margin-bottom: -18px;">
+  <div class="header_container">
+    <el-form
+      :inline="true"
+      :model="form"
+      ref="form"
+      style="margin-bottom: -18px"
+    >
       <el-form-item label="分类" prop="goods_category_id">
         <el-cascader
           v-model="form.list"
           placeholder="试试搜索：分类"
           :options="options"
           :show-all-levels="false"
-          style="width: 220px;"
+          style="width: 220px"
           filterable
           clearable
         ></el-cascader>
@@ -29,11 +34,14 @@
           icon="el-icon-search"
           :disabled="loading"
           @click="handleFormSubmit(true)"
-        >查询</el-button>
+          >查询</el-button
+        >
       </el-form-item>
 
       <el-form-item>
-        <el-button icon="el-icon-refresh" @click="handleFormReset">重置</el-button>
+        <el-button icon="el-icon-refresh" @click="handleFormReset"
+          >重置</el-button
+        >
       </el-form-item>
     </el-form>
   </div>
@@ -41,222 +49,298 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       form: {
         list: [],
-        keywords: '搜索',
-
+        keywords: "搜索",
       },
-      options: [{
-        value: 'zhinan',
-        label: '指南',
-        children: [{
-          value: 'shejiyuanze',
-          label: '设计原则',
-          children: [{
-            value: 'yizhi',
-            label: '一致'
-          }, {
-            value: 'fankui',
-            label: '反馈'
-          }, {
-            value: 'xiaolv',
-            label: '效率'
-          }, {
-            value: 'kekong',
-            label: '可控'
-          }]
-        }, {
-          value: 'daohang',
-          label: '导航',
-          children: [{
-            value: 'cexiangdaohang',
-            label: '侧向导航'
-          }, {
-            value: 'dingbudaohang',
-            label: '顶部导航'
-          }]
-        }]
-      }, {
-        value: 'zujian',
-        label: '组件',
-        children: [{
-          value: 'basic',
-          label: 'Basic',
-          children: [{
-            value: 'layout',
-            label: 'Layout 布局'
-          }, {
-            value: 'color',
-            label: 'Color 色彩'
-          }, {
-            value: 'typography',
-            label: 'Typography 字体'
-          }, {
-            value: 'icon',
-            label: 'Icon 图标'
-          }, {
-            value: 'button',
-            label: 'Button 按钮'
-          }]
-        }, {
-          value: 'form',
-          label: 'Form',
-          children: [{
-            value: 'radio',
-            label: 'Radio 单选框'
-          }, {
-            value: 'checkbox',
-            label: 'Checkbox 多选框'
-          }, {
-            value: 'input',
-            label: 'Input 输入框'
-          }, {
-            value: 'input-number',
-            label: 'InputNumber 计数器'
-          }, {
-            value: 'select',
-            label: 'Select 选择器'
-          }, {
-            value: 'cascader',
-            label: 'Cascader 级联选择器'
-          }, {
-            value: 'switch',
-            label: 'Switch 开关'
-          }, {
-            value: 'slider',
-            label: 'Slider 滑块'
-          }, {
-            value: 'time-picker',
-            label: 'TimePicker 时间选择器'
-          }, {
-            value: 'date-picker',
-            label: 'DatePicker 日期选择器'
-          }, {
-            value: 'datetime-picker',
-            label: 'DateTimePicker 日期时间选择器'
-          }, {
-            value: 'upload',
-            label: 'Upload 上传'
-          }, {
-            value: 'rate',
-            label: 'Rate 评分'
-          }, {
-            value: 'form',
-            label: 'Form 表单'
-          }]
-        }, {
-          value: 'data',
-          label: 'Data',
-          children: [{
-            value: 'table',
-            label: 'Table 表格'
-          }, {
-            value: 'tag',
-            label: 'Tag 标签'
-          }, {
-            value: 'progress',
-            label: 'Progress 进度条'
-          }, {
-            value: 'tree',
-            label: 'Tree 树形控件'
-          }, {
-            value: 'pagination',
-            label: 'Pagination 分页'
-          }, {
-            value: 'badge',
-            label: 'Badge 标记'
-          }]
-        }, {
-          value: 'notice',
-          label: 'Notice',
-          children: [{
-            value: 'alert',
-            label: 'Alert 警告'
-          }, {
-            value: 'loading',
-            label: 'Loading 加载'
-          }, {
-            value: 'message',
-            label: 'Message 消息提示'
-          }, {
-            value: 'message-box',
-            label: 'MessageBox 弹框'
-          }, {
-            value: 'notification',
-            label: 'Notification 通知'
-          }]
-        }, {
-          value: 'navigation',
-          label: 'Navigation',
-          children: [{
-            value: 'menu',
-            label: 'NavMenu 导航菜单'
-          }, {
-            value: 'tabs',
-            label: 'Tabs 标签页'
-          }, {
-            value: 'breadcrumb',
-            label: 'Breadcrumb 面包屑'
-          }, {
-            value: 'dropdown',
-            label: 'Dropdown 下拉菜单'
-          }, {
-            value: 'steps',
-            label: 'Steps 步骤条'
-          }]
-        }, {
-          value: 'others',
-          label: 'Others',
-          children: [{
-            value: 'dialog',
-            label: 'Dialog 对话框'
-          }, {
-            value: 'tooltip',
-            label: 'Tooltip 文字提示'
-          }, {
-            value: 'popover',
-            label: 'Popover 弹出框'
-          }, {
-            value: 'card',
-            label: 'Card 卡片'
-          }, {
-            value: 'carousel',
-            label: 'Carousel 走马灯'
-          }, {
-            value: 'collapse',
-            label: 'Collapse 折叠面板'
-          }]
-        }]
-      }, {
-        value: 'ziyuan',
-        label: '资源',
-        children: [{
-          value: 'axure',
-          label: 'Axure Components'
-        }, {
-          value: 'sketch',
-          label: 'Sketch Templates'
-        }, {
-          value: 'jiaohu',
-          label: '组件交互文档'
-        }]
-      }]
-    }
+      loading: false,
+      options: [
+        {
+          value: "zhinan",
+          label: "指南",
+          children: [
+            {
+              value: "shejiyuanze",
+              label: "设计原则",
+              children: [
+                {
+                  value: "yizhi",
+                  label: "一致",
+                },
+                {
+                  value: "fankui",
+                  label: "反馈",
+                },
+                {
+                  value: "xiaolv",
+                  label: "效率",
+                },
+                {
+                  value: "kekong",
+                  label: "可控",
+                },
+              ],
+            },
+            {
+              value: "daohang",
+              label: "导航",
+              children: [
+                {
+                  value: "cexiangdaohang",
+                  label: "侧向导航",
+                },
+                {
+                  value: "dingbudaohang",
+                  label: "顶部导航",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          value: "zujian",
+          label: "组件",
+          children: [
+            {
+              value: "basic",
+              label: "Basic",
+              children: [
+                {
+                  value: "layout",
+                  label: "Layout 布局",
+                },
+                {
+                  value: "color",
+                  label: "Color 色彩",
+                },
+                {
+                  value: "typography",
+                  label: "Typography 字体",
+                },
+                {
+                  value: "icon",
+                  label: "Icon 图标",
+                },
+                {
+                  value: "button",
+                  label: "Button 按钮",
+                },
+              ],
+            },
+            {
+              value: "form",
+              label: "Form",
+              children: [
+                {
+                  value: "radio",
+                  label: "Radio 单选框",
+                },
+                {
+                  value: "checkbox",
+                  label: "Checkbox 多选框",
+                },
+                {
+                  value: "input",
+                  label: "Input 输入框",
+                },
+                {
+                  value: "input-number",
+                  label: "InputNumber 计数器",
+                },
+                {
+                  value: "select",
+                  label: "Select 选择器",
+                },
+                {
+                  value: "cascader",
+                  label: "Cascader 级联选择器",
+                },
+                {
+                  value: "switch",
+                  label: "Switch 开关",
+                },
+                {
+                  value: "slider",
+                  label: "Slider 滑块",
+                },
+                {
+                  value: "time-picker",
+                  label: "TimePicker 时间选择器",
+                },
+                {
+                  value: "date-picker",
+                  label: "DatePicker 日期选择器",
+                },
+                {
+                  value: "datetime-picker",
+                  label: "DateTimePicker 日期时间选择器",
+                },
+                {
+                  value: "upload",
+                  label: "Upload 上传",
+                },
+                {
+                  value: "rate",
+                  label: "Rate 评分",
+                },
+                {
+                  value: "form",
+                  label: "Form 表单",
+                },
+              ],
+            },
+            {
+              value: "data",
+              label: "Data",
+              children: [
+                {
+                  value: "table",
+                  label: "Table 表格",
+                },
+                {
+                  value: "tag",
+                  label: "Tag 标签",
+                },
+                {
+                  value: "progress",
+                  label: "Progress 进度条",
+                },
+                {
+                  value: "tree",
+                  label: "Tree 树形控件",
+                },
+                {
+                  value: "pagination",
+                  label: "Pagination 分页",
+                },
+                {
+                  value: "badge",
+                  label: "Badge 标记",
+                },
+              ],
+            },
+            {
+              value: "notice",
+              label: "Notice",
+              children: [
+                {
+                  value: "alert",
+                  label: "Alert 警告",
+                },
+                {
+                  value: "loading",
+                  label: "Loading 加载",
+                },
+                {
+                  value: "message",
+                  label: "Message 消息提示",
+                },
+                {
+                  value: "message-box",
+                  label: "MessageBox 弹框",
+                },
+                {
+                  value: "notification",
+                  label: "Notification 通知",
+                },
+              ],
+            },
+            {
+              value: "navigation",
+              label: "Navigation",
+              children: [
+                {
+                  value: "menu",
+                  label: "NavMenu 导航菜单",
+                },
+                {
+                  value: "tabs",
+                  label: "Tabs 标签页",
+                },
+                {
+                  value: "breadcrumb",
+                  label: "Breadcrumb 面包屑",
+                },
+                {
+                  value: "dropdown",
+                  label: "Dropdown 下拉菜单",
+                },
+                {
+                  value: "steps",
+                  label: "Steps 步骤条",
+                },
+              ],
+            },
+            {
+              value: "others",
+              label: "Others",
+              children: [
+                {
+                  value: "dialog",
+                  label: "Dialog 对话框",
+                },
+                {
+                  value: "tooltip",
+                  label: "Tooltip 文字提示",
+                },
+                {
+                  value: "popover",
+                  label: "Popover 弹出框",
+                },
+                {
+                  value: "card",
+                  label: "Card 卡片",
+                },
+                {
+                  value: "carousel",
+                  label: "Carousel 走马灯",
+                },
+                {
+                  value: "collapse",
+                  label: "Collapse 折叠面板",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          value: "ziyuan",
+          label: "资源",
+          children: [
+            {
+              value: "axure",
+              label: "Axure Components",
+            },
+            {
+              value: "sketch",
+              label: "Sketch Templates",
+            },
+            {
+              value: "jiaohu",
+              label: "组件交互文档",
+            },
+          ],
+        },
+      ],
+    };
   },
-  created () {
-
-  },
+  created() {},
   methods: {
-    handleFormSubmit () {
-      console.log('123123');
-    }
-  }
-}
+    //提交
+    handleFormSubmit() {
+      console.log("123123");
+    },
+    //重置
+    handleFormReset() {
+      console.log("123123");
+    },
+  },
+};
 </script>
 <style lang="scss" scoped>
-.container {
+.header_container {
   padding: 20px;
   // padding-bottom: 30px;
   border-bottom: 1px solid #ccc;
--- src/views/goods/list/index.vue
+++ src/views/goods/list/index.vue
@@ -1,21 +1,419 @@
 <template>
   <div class="container">
+    <!-- 头部分类 -->
     <Header></Header>
-    <Main></Main>
+
+    <div class="fun">
+      <el-form :inline="true" size="small">
+        <el-form-item v-if="tabPane !== 'delete'">
+          <el-button icon="el-icon-plus" @click="handleCreate"
+            >新增商品</el-button
+          >
+        </el-form-item>
+
+        <el-form-item v-if="tabPane === 'stock'">
+          <el-button icon="el-icon-top" @click="handleStatus(null, 1)"
+            >上架</el-button
+          >
+        </el-form-item>
+
+        <el-form-item v-if="tabPane === 'sale'">
+          <el-button icon="el-icon-bottom" @click="handleStatus(null, 0)"
+            >下架</el-button
+          >
+        </el-form-item>
+
+        <el-form-item v-if="tabPane !== 'delete'">
+          <el-dropdown placement="bottom" :show-timeout="50">
+            <el-button>
+              <i class="el-icon-medal" />
+              <span>推荐</span>
+              <i class="el-icon-arrow-down cs-pl-5" />
+            </el-button>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item @click.native="handleRecommend(null, 1)"
+                >设为推荐</el-dropdown-item
+              >
+              <el-dropdown-item @click.native="handleRecommend(null, 0)"
+                >取消推荐</el-dropdown-item
+              >
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-form-item>
+
+        <el-form-item v-if="tabPane !== 'delete'">
+          <el-dropdown placement="bottom" :show-timeout="50">
+            <el-button>
+              <i class="el-icon-star-off" />
+              <span>新品</span>
+              <i class="el-icon-arrow-down cs-pl-5" />
+            </el-button>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item @click.native="handleNew(null, 1)"
+                >设为新品</el-dropdown-item
+              >
+              <el-dropdown-item @click.native="handleNew(null, 0)"
+                >取消新品</el-dropdown-item
+              >
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-form-item>
+
+        <el-form-item v-if="tabPane !== 'delete'">
+          <el-dropdown placement="bottom" :show-timeout="50">
+            <el-button>
+              <i class="el-icon-sunny" />
+              <span>热卖</span>
+              <i class="el-icon-arrow-down cs-pl-5" />
+            </el-button>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item @click.native="handleHot(null, 1)"
+                >设为热卖</el-dropdown-item
+              >
+              <el-dropdown-item @click.native="handleHot(null, 0)"
+                >取消热卖</el-dropdown-item
+              >
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button-group>
+            <el-button icon="el-icon-delete" @click="handleDelete(null, true)">
+              <span>{{ tabPane === "delete" ? "彻底删除" : "删除" }}</span>
+            </el-button>
+
+            <el-button
+              v-if="tabPane === 'delete'"
+              icon="el-icon-refresh-left"
+              @click="handleDelete(null, false)"
+              >恢复</el-button
+            >
+          </el-button-group>
+        </el-form-item>
+      </el-form>
+    </div>
+
+    <div class="goods_list">
+      <el-tabs v-model="tabPane" @tab-click="handleTabsEdit">
+        <el-tab-pane
+          :key="index"
+          v-for="(item, index) in tabList"
+          :label="item"
+          :name="index"
+        >
+          <el-table
+            ref="multipleTable"
+            :data="tableData"
+            tooltip-effect="dark"
+            style="width: 100%"
+            @selection-change="handleSelectionChange"
+          >
+            <el-table-column type="selection" width="55"> </el-table-column>
+            <el-table-column label="商品" sortable="custom" min-width="150">
+              <template slot-scope="scope">
+                <el-image
+                  class="goods-image"
+                  @click="handleView(scope.row.goods_id)"
+                  :src="img"
+                  fit="contain"
+                  lazy
+                />
+                <div class="goods_info">
+                  <div class="name">{{ scope.row.date }}</div>
+                  <div class="des">这是物品的描述</div>
+                  <div class="ind">物品介绍</div>
+                </div>
+              </template>
+            </el-table-column>
+
+            <el-table-column
+              sortable="custom"
+              prop="name"
+              label="本店价"
+              width="120"
+            >
+            </el-table-column>
+
+            <el-table-column
+              sortable="custom"
+              prop="address"
+              label="库存"
+              show-overflow-tooltip
+            >
+            </el-table-column>
+
+            <el-table-column
+              sortable="custom"
+              prop="address"
+              label="总销量"
+              show-overflow-tooltip
+            >
+            </el-table-column>
+
+            <el-table-column
+              sortable="custom"
+              prop="address"
+              label="排序值"
+              align="center"
+              show-overflow-tooltip
+            >
+              <template slot-scope="scope">
+                <el-input-number
+                  v-if="tabPane !== 'delete'"
+                  v-model="scope.row.sort"
+                  style="width: 88px"
+                  size="mini"
+                  controls-position="right"
+                  :min="0"
+                  :max="255"
+                  @change="handleSort(scope.$index)"
+                >
+                </el-input-number>
+                <span v-else>
+                  {{ scope.row.sort }}
+                </span>
+              </template>
+            </el-table-column>
+
+            <el-table-column label="操作" align="center">
+              <template slot-scope="scope">
+                <div class="goods-text">
+                  <p v-if="tabPane !== 'delete'">
+                    <el-link
+                      class="button"
+                      type="primary"
+                      @click="handleEdit(scope.row.goods_id)"
+                      :underline="false"
+                      >编辑商品</el-link
+                    >
+                  </p>
+
+                  <p v-if="tabPane !== 'delete'">
+                    <el-link
+                      class="button"
+                      type="primary"
+                      @click="handleCopy(scope.row.goods_id)"
+                      :underline="false"
+                      >复制商品</el-link
+                    >
+                  </p>
+
+                  <p v-if="tabPane !== 'delete'">
+                    <el-link
+                      class="button"
+                      type="primary"
+                      @click="
+                        handleStatus(scope.$index, Number(!scope.row.status))
+                      "
+                      :underline="false"
+                      >{{ scope.row.status ? "下架" : "上架" }}</el-link
+                    >
+                  </p>
+
+                  <p>
+                    <el-link
+                      class="button"
+                      type="primary"
+                      @click="handleDelete(scope.$index, true)"
+                      :underline="false"
+                      >{{ tabPane === "delete" ? "彻底删除" : "删除" }}</el-link
+                    >
+                  </p>
+
+                  <p v-if="tabPane === 'delete'">
+                    <el-link
+                      class="button"
+                      type="primary"
+                      @click="handleDelete(scope.$index, false)"
+                      :underline="false"
+                      >恢复</el-link
+                    >
+                  </p>
+                </div>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+
+    <!-- 翻页 -->
+    <page-footer
+      slot="footer"
+      :loading="loading"
+      :page-no="page.page_no"
+      :page-size="page.page_size"
+      :total="pageTotal"
+      @change="handlePaginationChange"
+    />
   </div>
 </template>
 
 <script>
-import Header from './components/header.vue'
-import Main from './components/main.vue'
+import Header from "./components/header.vue";
+import PageFooter from "../../components/page-footer/index.vue";
 export default {
   components: {
     Header,
-    Main
-  }
-}
+    PageFooter,
+  },
+  data() {
+    return {
+      tabPane: "sale",
+      tabList: { sale: "出售中", stock: "已下架", delete: "回收站" },
+      editableTabsValue: "2",
+      editableTabs: [
+        {
+          title: "Tab 1",
+          name: "1",
+          content: "Tab 1 content",
+        },
+        {
+          title: "Tab 2",
+          name: "2",
+          content: "Tab 2 content",
+        },
+      ],
+      tabIndex: 2,
+      tableData: [
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-08",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-06",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+      ],
+      multipleSelection: [],
+      img: "https://demo.careyshop.cn/api/v1/storage.html?method=get.storage.thumb&code=goods_image_x480&url=aliyun.oss.careyshop.cn%2Fuploads%2Ffiles%2F20191230%2F32dcbebd-ebad-4d41-96d4-196fd7fe9ac7.jpg%3Ftype%3Daliyun",
+      page: {
+        page_no: 1,
+        page_size: 0,
+      },
+      pageTotal: 0,
+    };
+  },
+  created() {},
+  methods: {
+    // 新增商品
+    handleCreate() {
+      console.log("3333");
+    },
+    // 处理上架下架
+    handleStatus() {
+      console.log("333");
+    },
+    //设置为推荐
+    handleRecommend() {},
+    //设置为新品
+    handleNew() {},
+    //设置为热卖
+    handleHot() {},
+    // 删除
+    handleDelete() {},
+    //tab点击事件
+    handleTabsEdit(tab) {
+      let config = { status: 1, is_delete: 0 };
+      switch (tab.name) {
+        case "stock":
+          config.status = 0;
+          break;
+
+        case "delete":
+          config.is_delete = 1;
+          break;
+      }
+    },
+    // 选中数据项
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+    // 编辑商品
+    handleEdit() {},
+    //复制商品
+    handleCopy() {},
+    //上下架商品
+    handleStatus() {},
+    //删除or回复商品
+    handleDelete() {},
+    //排序
+    handleSort() {},
+    handlePaginationChange(val) {
+      this.page = val;
+      if ((val.page_no - 1) * val.page_size > this.pageTotal) {
+        return;
+      }
+
+      this.$nextTick(() => {
+        this.$refs.header.handleFormSubmit();
+      });
+    },
+  },
+};
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+.container {
+  padding-bottom: 53px;
+}
+.fun {
+  padding: 20px;
+  padding-bottom: 0;
+  border-bottom: 1px solid #ccc;
+}
+.goods_list {
+  padding: 20px;
+}
+.goods-text {
+  p {
+    margin: 0;
+  }
 
+  .button {
+    padding: 0;
+    font-size: 13px;
+  }
+}
+.goods-image {
+  float: left;
+  width: 150px;
+  height: 150px;
+}
+.goods_info {
+  float: left;
+  margin-top: 30px;
+  margin-left: 10px;
+  .name {
+    font-size: 18px;
+  }
+}
 </style>
